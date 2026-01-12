68222fe61eaf4b275669cf83188e67428738cba4
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2026-01-12 23:38:42 +0100

    seperated printing out the routes into a function AAAAAAAAAAAAAAAAA

--- Program.cs
+++ Program.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
-public class Corridor
+﻿public class Corridor
 {
-    public int start { get; set; }
     public int end { get; set; }
     public int length { get; set; }
-    public Corridor(int start, int end, int length)
+    public Corridor(int end, int length)
     {
-        this.start = start;
         this.end = end;
         this.length = length;
     }
@@ -17,6 +11,20 @@ public class Corridor
 
 class Program
 {
+
+    static void printCorridors(List<Corridor>[] corridors)
+    {
+        int j = 0;
+        foreach (List<Corridor> whatever in corridors)
+        {
+            System.Console.WriteLine($"hypercity {j}");
+            foreach (Corridor cor in whatever)
+            {
+                System.Console.WriteLine($"    target: {cor.end},\n    cost: {cor.length}\n");
+            }
+            j++;
+        }
+    }
     static void Main(string[] args)
     {
 
@@ -29,34 +37,24 @@ class Program
         m = int.Parse(header[1]); // liczba korytarzy
         k = int.Parse(header[2]); // liczba max. użyć korytarzy.
 
-        // each index is a city and contains corridors ending in dat city??
-        List<Corridor>[] evil = new List<Corridor>[n];
-        for (int idx = 0; idx < n; idx++)
+        List<Corridor>[] routes = new List<Corridor>[n];
+
+        for (int i = 0; i < n; i++)
         {
-            evil[idx] = new List<Corridor>();
+            routes[i] = new List<Corridor>();
         }
-        // parsing korytarzy
+
         for (int i = 1; i <= m; i++)
         {
             string[] split = lines[i].Split();
             int start = int.Parse(split[0]);
             int end = int.Parse(split[1]);
             int len = int.Parse(split[2]);
-            Corridor cor = new Corridor(start, end, len);
+            Corridor cor = new Corridor(end, len);
 
-            evil[start - 1].Add(cor);
+            routes[start - 1].Add(cor);
         }
 
-        int j = 0;
-        foreach (List<Corridor> whatever in evil)
-        {
-            System.Console.WriteLine($"evil {j + 1}");
-            foreach (Corridor cor in whatever)
-            {
-                System.Console.WriteLine($"{cor.start}, {cor.end}, {cor.length}");
-            }
-            j++;
-        }
-
+        printCorridors(routes);
     }
 }
