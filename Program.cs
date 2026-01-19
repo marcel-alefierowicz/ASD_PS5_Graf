e24e0f8521f71dca8608db3732114871db3b7886
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2026-01-19 20:25:31 +0100

    implemented modified djikstra's alg, pending implementation of tracking predecessors

--- Program.cs
+++ Program.cs
@@ -1,60 +1,95 @@
-﻿public class Corridor
-{
-    public int end { get; set; }
-    public int length { get; set; }
-    public Corridor(int end, int length)
-    {
-        this.end = end;
-        this.length = length;
-    }
-}
+﻿// https://www.geeksforgeeks.org/dsa/dijkstras-shortest-path-algorithm-greedy-algo-7/
+using System.Diagnostics;
 
 class Program
 {
-
-    static void printCorridors(List<Corridor>[] corridors)
+    static void addEdge(List<List<(int, int)>> list, int start, int end, int weight)
     {
-        int j = 0;
-        foreach (List<Corridor> whatever in corridors)
-        {
-            System.Console.WriteLine($"hypercity {j}");
-            foreach (Corridor cor in whatever)
-            {
-                System.Console.WriteLine($"    target: {cor.end},\n    cost: {cor.length}\n");
-            }
-            j++;
-        }
+        list[start].Add((end, weight));
+        list[end].Add((start, weight));
     }
+
+    static int modifiedDjikstra(List<List<(int, int)>> adj, int start, int target, int k)
+    {
+        PriorityQueue<(int node, int usedZeroEdges), int> pq = new(); // MinHeap, lower value takes prio
+        // storing tuples in the pq containing the node & the number of zero-weight edges used at that given point.
+
+        int n = adj.Count;
+        int[,] dist = new int[n, k + 1];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= k; j++)
+            {
+                dist[i, j] = int.MaxValue;
+            }
+        }
+
+        dist[start, 0] = 0;
+        pq.Enqueue((start, 0), 0);
+
+        int bestTarget = int.MaxValue;
+
+        while (pq.Count > 0)
+        {
+            pq.TryDequeue(out (int node, int usedZeroEdges) state, out int d);
+            int u = state.node; // current node
+            int usedZero = state.usedZeroEdges; // used zeroes at this point
+
+            if (d > dist[u, usedZero]) continue;
+            foreach ((int, int) edge in adj[u])
+            {
+                (int v, int weight) = edge;
+                int nextUsedZero = usedZero + (weight == 0 ? 1 : 0);
+                if (nextUsedZero > k) continue;
+
+                int nd = d + weight;
+                if (nd < dist[v, nextUsedZero])
+                {
+                    dist[v, nextUsedZero] = nd;
+                    pq.Enqueue((v, nextUsedZero), nd);
+                    if (v == target && nd < bestTarget) bestTarget = nd;
+                }
+            }
+        }
+
+        if (bestTarget == int.MaxValue) return -1;
+        return bestTarget;
+    }
+
     static void Main(string[] args)
     {
 
         string[] lines = File.ReadAllLines(args[0]);
         string[] header = lines[0].Split(' ');
 
-        int n, m, k;
+        int n = int.Parse(header[0]); // liczba hypermiast
+        int m = int.Parse(header[1]); // liczba korytarzy
+        int k = int.Parse(header[2]); // liczba max. użyć korytarzy.
 
-        n = int.Parse(header[0]); // liczba hypermiast
-        m = int.Parse(header[1]); // liczba korytarzy
-        k = int.Parse(header[2]); // liczba max. użyć korytarzy.
-
-        List<Corridor>[] routes = new List<Corridor>[n];
-
+        Stopwatch sw = new();
+        sw.Start();
+        List<List<(int, int)>> adj = new List<List<(int, int)>>(n);
         for (int i = 0; i < n; i++)
         {
-            routes[i] = new List<Corridor>();
+            adj.Add(new List<(int, int)>());
         }
 
         for (int i = 1; i <= m; i++)
         {
             string[] split = lines[i].Split();
-            int start = int.Parse(split[0]);
-            int end = int.Parse(split[1]);
+            int start = int.Parse(split[0]) - 1;
+            int end = int.Parse(split[1]) - 1;
             int len = int.Parse(split[2]);
-            Corridor cor = new Corridor(end, len);
 
-            routes[start - 1].Add(cor);
+
+            addEdge(adj, start, end, len);
         }
 
-        printCorridors(routes);
+        int shortest = modifiedDjikstra(adj, 0, n - 1, k);
+        sw.Stop();
+        System.Console.WriteLine($"czas:\n   {sw.ElapsedMilliseconds}ms");
+        System.Console.WriteLine($"wynik:\n   1 --> {n}: {shortest}");
     }
+
+
 }
