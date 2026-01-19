b3f670d8ba00db0dd4d7330f568e577d7532bba4
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2026-01-19 21:30:28 +0100

    implemented backtracking after main loop to find the path taken

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// https://www.geeksforgeeks.org/dsa/dijkstras-shortest-path-algorithm-greedy-algo-7/
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 class Program
 {
@@ -9,33 +8,48 @@ class Program
         list[end].Add((start, weight));
     }
 
-    static int modifiedDjikstra(List<List<(int, int)>> adj, int start, int target, int k)
+    static (int distance, List<int> path) modifiedDjikstra(List<List<(int, int)>> adj, int start, int target, int k)
     {
         PriorityQueue<(int node, int usedZeroEdges), int> pq = new(); // MinHeap, lower value takes prio
         // storing tuples in the pq containing the node & the number of zero-weight edges used at that given point.
 
         int n = adj.Count;
         int[,] dist = new int[n, k + 1];
+        int[,] prevNode = new int[n, k + 1];
+        int[,] prevZero = new int[n, k + 1];
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j <= k; j++)
             {
                 dist[i, j] = int.MaxValue;
+                prevNode[i, j] = -1;
+                prevZero[i, j] = -1;
             }
         }
 
         dist[start, 0] = 0;
         pq.Enqueue((start, 0), 0);
 
-        int bestTarget = int.MaxValue;
+        int bestTargetDist = int.MaxValue;
+        int bestTargetZero = -1;
 
         while (pq.Count > 0)
         {
-            pq.TryDequeue(out (int node, int usedZeroEdges) state, out int d);
+            pq.TryDequeue(out (int node, int usedZeroEdges) state, out int d); // d - najmniej kosztowna droga do tego node'a
             int u = state.node; // current node
             int usedZero = state.usedZeroEdges; // used zeroes at this point
 
             if (d > dist[u, usedZero]) continue;
+
+            // Dijkstra property (on this expanded state-space):
+            // the first time we *dequeue* a state for `target`, it is globally optimal.
+            if (u == target)
+            {
+                bestTargetDist = d;
+                bestTargetZero = usedZero;
+                break;
+            }
+
             foreach ((int, int) edge in adj[u])
             {
                 (int v, int weight) = edge;
@@ -46,14 +60,30 @@ class Program
                 if (nd < dist[v, nextUsedZero])
                 {
                     dist[v, nextUsedZero] = nd;
+                    prevNode[v, nextUsedZero] = u;
+                    prevZero[v, nextUsedZero] = usedZero;
                     pq.Enqueue((v, nextUsedZero), nd);
-                    if (v == target && nd < bestTarget) bestTarget = nd;
                 }
             }
+
         }
 
-        if (bestTarget == int.MaxValue) return -1;
-        return bestTarget;
+        if (bestTargetDist == int.MaxValue) return (-1, new List<int>());
+
+        List<int> path = new();
+        int currNode = target;
+        int currZero = bestTargetZero;
+        while (currNode != -1)
+        {
+            path.Add(currNode);
+            int pNode = prevNode[currNode, currZero];
+            int pZero = prevZero[currNode, currZero];
+            currNode = pNode;
+            currZero = pZero;
+        }
+        path.Reverse();
+
+        return (bestTargetDist, path);
     }
 
     static void Main(string[] args)
@@ -85,11 +115,22 @@ class Program
             addEdge(adj, start, end, len);
         }
 
-        int shortest = modifiedDjikstra(adj, 0, n - 1, k);
+        (int shortest, List<int> path) = modifiedDjikstra(adj, 0, n - 1, k);
         sw.Stop();
         System.Console.WriteLine($"czas:\n   {sw.ElapsedMilliseconds}ms");
         System.Console.WriteLine($"wynik:\n   1 --> {n}: {shortest}");
+        if (shortest != -1)
+        {
+            System.Console.WriteLine($"trasa:\n   {string.Join(" -> ", path.Select(x => x + 1))}");
+        }
     }
 
-
 }
+
+/*
+
+Złożoność czasowa:
+    0((n * (k+1) + m * (k+1)) * log(n*(k+1))) = 
+    = 0( (n+m) * (k+1) * log(n * (k+1)) ) 
+    
+*/
